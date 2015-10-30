92418512bb4811df93f01c9c6ec04e9bc7b9cbc1
William Heng <william@williamheng.com>
2015-10-30 15:11:15 +0000

    Add Location header field when new resource is created

--- src/main/java/com/bjss/william/employees/controller/EmployeesController.java
+++ src/main/java/com/bjss/william/employees/controller/EmployeesController.java
@@ -3,12 +3,19 @@ package com.bjss.william.employees.controller;
 import com.bjss.william.employees.model.Employee;
 import com.bjss.william.employees.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class EmployeesController {
@@ -41,7 +48,7 @@ public class EmployeesController {
         Employee employee = employeeService.getEmployeeById(Integer.parseInt(id));
 
         if (employee == null) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         } else {
             return new ResponseEntity<>(employee, HttpStatus.OK);
         }
@@ -54,10 +61,44 @@ public class EmployeesController {
             consumes = MediaType.APPLICATION_JSON_VALUE
     )
     @ResponseBody
-    public ResponseEntity<Employee> addEmployee(
+    public ResponseEntity<EmployeeCreated> addEmployee(
+            HttpServletRequest httpServletRequest,
             @RequestBody Employee employee
     ) {
         employeeService.addEmployee(employee);
-        return new ResponseEntity<>(employee, HttpStatus.CREATED);
+
+        if (employee.getEmployeeNumber() == 0) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        String newLocation = String.format("%s/%d", httpServletRequest.getRequestURL(), employee.getEmployeeNumber());
+        try {
+            URI uri = new URI(newLocation);
+            HttpHeaders httpResponseHeaders = new HttpHeaders();
+            httpResponseHeaders.setLocation(uri);
+
+            return new ResponseEntity<>(new EmployeeCreated(employee.getEmployeeNumber(), newLocation), httpResponseHeaders, HttpStatus.CREATED);
+        } catch (URISyntaxException e) {
+            return new ResponseEntity<>(HttpStatus.SERVICE_UNAVAILABLE);
+        }
+
+    }
+}
+
+class EmployeeCreated  {
+    private int employeedNumber;
+    private String location;
+
+    public EmployeeCreated(int employeedNumber, String location) {
+        this.employeedNumber = employeedNumber;
+        this.location = location;
+    }
+
+    public int getEmployeedNumber() {
+        return employeedNumber;
+    }
+
+    public String getLocation() {
+        return location;
     }
 }
